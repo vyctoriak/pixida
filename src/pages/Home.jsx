28140c052733be1a6066f2616e433373ea5bdde3
vyctoria karina <vyctoriak@gmail.com>
2023-05-13 23:33:11 +0200

    home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import SearchBar from "../components/SearchBar/SearchBar";
+
+const Home = () => {
+  return (
+    <div className="conteiner">
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Home;
