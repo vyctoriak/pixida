35d41bf6acb57c8f54e931e9398a3a6056d0551a
vyctoria karina <vyctoriak@gmail.com>
2023-05-16 09:40:38 +0200

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
