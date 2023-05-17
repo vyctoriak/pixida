83bb8469911ca7ea729497821587b3740d0cdc93
vyctoria karina <vyctoriak@gmail.com>
2023-05-17 11:20:16 +0200

    add single art page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Art from "./components/Art/Art";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/art/:id" element={<Art />} />
       </Routes>
     </BrowserRouter>
   );
