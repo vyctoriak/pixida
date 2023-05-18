19cfb561cfd0a2d6104f77a599813eb262885ece
vyctoria karina <vyctoriak@gmail.com>
2023-05-18 09:42:18 +0200

    tests for pagination component

---
+++ src/specs/Pagination.spec.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Pagination from "../components/Pagination/Pagination";
+
+describe("Pagination", () => {
+  it("renders without errors", () => {
+    render(
+      <Pagination currentPage={1} totalPages={5} onPageChange={() => {}} />
+    );
+  });
+
+  it("calls onPageChange with previous page number when previous page is clicked", () => {
+    const onPageChangeMock = jest.fn();
+    const { getByAltText } = render(
+      <Pagination
+        currentPage={3}
+        totalPages={5}
+        onPageChange={onPageChangeMock}
+      />
+    );
+    const leftArrowIcon = getByAltText("left arrow icon");
+
+    fireEvent.click(leftArrowIcon);
+
+    expect(onPageChangeMock).toHaveBeenCalledWith(2);
+  });
+
+  it("calls onPageChange with next page number when next page is clicked", () => {
+    const onPageChangeMock = jest.fn();
+    const { getByAltText } = render(
+      <Pagination
+        currentPage={3}
+        totalPages={5}
+        onPageChange={onPageChangeMock}
+      />
+    );
+    const rightArrowIcon = getByAltText("right arrow icon");
+
+    fireEvent.click(rightArrowIcon);
+
+    expect(onPageChangeMock).toHaveBeenCalledWith(4);
+  });
+
+  it("calls onPageChange with correct page number when a page is clicked", () => {
+    const onPageChangeMock = jest.fn();
+    const { getByText } = render(
+      <Pagination
+        currentPage={3}
+        totalPages={5}
+        onPageChange={onPageChangeMock}
+      />
+    );
+    const pageThree = getByText("3");
+
+    fireEvent.click(pageThree);
+
+    expect(onPageChangeMock).toHaveBeenCalledWith(3);
+  });
+});
