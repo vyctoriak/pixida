f54c95c4e9a36bb31143346a09c2111fc23e8745
vyctoria karina <vyctoriak@gmail.com>
2023-05-19 10:44:14 +0200

    add shadow to the card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,9 @@ const Card = ({ imageUrl, artistName, title }) => {
     <div
       data-testid="card-item"
       className="card-item"
-      style={{ backgroundImage: `url(${imageUrl})` }}
+      style={{
+        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0) 34.32%, rgba(0, 0, 0, 0.72) 72.5%), url(${imageUrl})`,
+      }}
     >
       <h1>{title ? title : "Untitled"}</h1>
       <div className="author-name">
